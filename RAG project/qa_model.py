dcb32c05f88133c60946ea9aebaa97ff4e2111b4
mannatkaurgithub <159148464+mannatkaurgithub@users.noreply.github.com>
2024-07-03 21:59:50 +0530

    Delete RAG project directory

--- RAG project/qa_model.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-def generate_answer(question, context, model_name='deepset/roberta-base-squad2'):
-    qa_pipeline = pipeline('question-answering', model=model_name)
-    result = qa_pipeline(question=question, context=context)
-    return result['answer']
